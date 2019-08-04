b8e1e911b325b87cd8aa95665b5747cb3844dc6d
elvinMark <38699075+elvinMark@users.noreply.github.com>
2019-08-04 15:21:05 +0900

    Add files via upload

--- mondai1.c
+++ mondai1.c
@@ -4,37 +4,56 @@
 
 #define STR_SIZE 21
 
+//Define the structure of the tree nodes
 typedef struct tnode{
   char data[STR_SIZE];
   struct tnode* right;
   struct tnode* left;
 }tnode;
 
+//function that create the tree nodes
 tnode* createTnode(char* str);
+//function that insert tree node in the binary tree
 void insertTerm(tnode** head,tnode *node);
+//function that print the binary tree
 void printTree(tnode* head);
+//function that free the heap used for the binary tree
 void freeTree(tnode* head);
 
 int main(int argc, char* args[]){
+  //we define the main variables used in this program
+  //head is the pointer to the root of the tree
+  //temp is just a temporal pointer that we will use later
   tnode* head,*temp;
+  //buffer will be used to read the strings from the file
   char buffer[STR_SIZE];
+  //p is the file pointer
   FILE *p;
+  //initialize head as empty (NULL)
   head = NULL;
+
   if(argc<2){
     fprintf(stderr,"Please introduce the name of the file");
     return -1;
   }
+  //we read the file which filename was specified in the pipe
   p = fopen(args[1],"r");
+  //we use a loop to read all the words from the file and 
+  //insert them in binary tree
   while(fgets(buffer,STR_SIZE,p)){
     buffer[strlen(buffer)-1] = '\0';
     temp = createTnode(buffer);
     insertTerm(&head,temp);
   }
+  //finally we print the tree
   printTree(head);
+  //and free the mmeory used by tree
   freeTree(head);
   return 0;
 }
 
+//create a new tree node using malloc
+//and initializing the pointer to right and left as NULL
 tnode* createTnode(char* str){
   tnode* out;
   out = (tnode*) malloc(sizeof(tnode));
@@ -44,6 +63,7 @@ tnode* createTnode(char* str){
   return out;
 }
 
+//insert the a node in the binary tree by recursion
 void insertTerm(tnode** head,tnode* node){
   if(*head == NULL){
     *head = node;
@@ -56,6 +76,10 @@ void insertTerm(tnode** head,tnode* node){
   }
 }
 
+//print the binary tree by recursion
+//print the left tree, 
+//then the data in current node and finaly 
+//print the right tree
 void printTree(tnode* head){
   if(head==NULL)
     return;
@@ -65,6 +89,7 @@ void printTree(tnode* head){
  
 }
 
+//free the memory occupied by the tree by recursion
 void freeTree(tnode *head){
   tnode* r;
   tnode* l;
--- mondai2.c
+++ mondai2.c
@@ -5,11 +5,13 @@
 #define STR_SIZE 22
 #define BUFF_SIZE 1005
 
+//Define the structure of the linked list nodes
 typedef struct lnode{
   int data;
   struct lnode* next;
 }lnode;
 
+//Define the structure of the tree nodes
 typedef struct tnode{
   char data[STR_SIZE];
   struct tnode* right;
@@ -19,27 +21,47 @@ typedef struct tnode{
 }tnode;
 
 
+//function that creates a new tree node
 tnode* createTnode(char* str);
+//function that inserts a tree node in the binary tree
 void insertTerm(tnode** head,tnode *node);
+//function that prints the binary tree
 void printTree(tnode* head);
+//function that frees the memory used by the binary tree
 void freeTree(tnode* head);
+//function that creates a new list node
 lnode* createLnode(int id);
+//function that inserts the node into the linked list (at the end)
 void insertLnode(lnode** head,lnode** last,lnode* new);
+//function that prints the linked list
 void printList(lnode* head);
+//function that frees the memory occupied by the linked list
 void freeList(lnode* head);
+//function that creates a new tree node and insert a new list node to 
+//its linked list and finally insert that tree node in the binary tree
 void insertTermAndList(tnode** head,char* str,int id);
+//function that search an specific data in the binary tree
 tnode* search(tnode* head,char* query);
 
 int main(int argc, char* args[]){
+  //head is the pointer to the root of the binary tree
   tnode* head;
+  //This node will be used when we want to a new linked list node into 
+  //certain linked list
   lnode *new_node;
+  //temporal pointer that will be used later
   tnode* aux;
+  //buffer will be used to read strings later on
   char buffer[BUFF_SIZE];
+  //char pointer that will used for the string tokenizer
   char *token;
+  //flag will control whether we are reading an id or words.
   int flag;
   int id;
+  //file pointer
   FILE *p;
 
+  //initialize the binary tree as empty (NULL)
   head = NULL;
 
   if(argc<2){
@@ -47,7 +69,10 @@ int main(int argc, char* args[]){
     return -1;
   }
 
+  //read the file wichi filename is specified in the pipe
   p = fopen(args[1],"r");
+  //read each line of the file and tokenize it. the first token will correspond to the id
+  // and the rest of the tokens will be words that are going to be inserted in the tree
   while(fgets(buffer,BUFF_SIZE,p)){
     buffer[strlen(buffer)-1] = '\0';
     token = strtok(buffer,"\t");
@@ -59,10 +84,13 @@ int main(int argc, char* args[]){
         flag = 0;
       }
       else{
+        //we first check wheteher this word is already in the tree or not
         aux = search(head,token);
+        //if that word was not already in the tree then insert it along with the id
         if(aux==NULL)
           insertTermAndList(&head,token,id);
         else{
+          //if that word was already in the tree, then just insert the new id into its linked list
           new_node = createLnode(id);
           insertLnode(&(aux->info),&(aux->info_last),new_node);
         }
@@ -71,6 +99,7 @@ int main(int argc, char* args[]){
     }
   }
 
+  //read the query from the user and print the result
   scanf("%s",buffer);
   if((aux = search(head,buffer))==NULL)
     printf("Not found\n");
@@ -82,6 +111,8 @@ int main(int argc, char* args[]){
   return 0;
 }
 
+//create a new tree node and initialize the pointer to the right and left as NULL. 
+//Also the pointers used for the linked list initialize as NULL since the list is empty
 tnode* createTnode(char* str){
   tnode* out;
   out = (tnode*) malloc(sizeof(tnode));
@@ -93,6 +124,7 @@ tnode* createTnode(char* str){
   return out;
 }
 
+//insert a new tree node in the binary tree by recursion
 void insertTerm(tnode** head,tnode* node){
   if(*head == NULL){
     *head = node;
@@ -105,6 +137,7 @@ void insertTerm(tnode** head,tnode* node){
   }
 }
 
+//print the binary tree also by recursion
 void printTree(tnode* head){
   if(head==NULL)
     return;
@@ -113,6 +146,7 @@ void printTree(tnode* head){
   printTree(head->right); 
 }
 
+//free the binary tree by recursion
 void freeTree(tnode *head){
   tnode* r;
   tnode* l;
@@ -126,6 +160,8 @@ void freeTree(tnode *head){
   freeTree(l);
 }
 
+//create a new list node with the specified input and initialize 
+//the next element pointer as NULL
 lnode* createLnode(int id){
   lnode* new;
   new = (lnode *)malloc(sizeof(lnode));
@@ -133,6 +169,10 @@ lnode* createLnode(int id){
   new->next = NULL;
   return new;
 }
+
+//insert a new node in the list at the end using the 
+//pointer last which is always pointing at the last element of the list
+//of course if the list is empty we initialize head and last as the new element
 void insertLnode(lnode** head,lnode** last,lnode* new){
   if(*head == NULL){
     *head = new;
@@ -145,12 +185,16 @@ void insertLnode(lnode** head,lnode** last,lnode* new){
     }
   }
 }
+
+//print the list by recursion
 void printList(lnode* head){
   while(head!=NULL){
     printf("%d\n",head->data);
     head = head->next;
   }
 }
+
+//free the memory used by the list by recursion
 void freeList(lnode* head){
   lnode* temp;
   while(head!=NULL){
@@ -159,6 +203,9 @@ void freeList(lnode* head){
     head = temp;
   }
 }
+
+//create a new tree node and also insert the id into its linked list.
+//and insert this node into the binary tree
 void insertTermAndList(tnode** head,char* str,int id){
   tnode* new_n;
   lnode* new_l;
@@ -176,6 +223,8 @@ void insertTermAndList(tnode** head,char* str,int id){
       insertTerm(&((*head)->left),new_n);
   }
 }
+
+//search in the binary tree by recursion
 tnode* search(tnode* head,char* query){
   tnode* out = NULL;
   if(head == NULL)
